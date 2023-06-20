abc839cb7f42cc74b07c1950ba247cb0217dd835
y0329 <kida_hirokazu@tuta.io>
2023-06-20 14:08:04 +0900

    練習第一問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import pygame as pg
 
 
@@ -6,8 +7,18 @@ WIDTH, HEIGHT = 1600, 900
 
 
 def main():
+    """
+    TEST
+    """
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bonb = pg.Surface((20,20))# 練習1
+    pg.draw.circle(bonb,(255,0,0),(10,10),10)# 練習1
+    bonb.set_colorkey((0,0,0))
+    x = random.randint(0,WIDTH)
+    y = random.randint(0,HEIGHT)
+    bonb_rect = bonb.get_rect()
+    bonb_rect.center = x , y
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
@@ -20,6 +31,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bonb,bonb_rect)# 練習1
         pg.display.update()
         tmr += 1
         clock.tick(10)
