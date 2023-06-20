3dd6265e2e72356263614a9ed2c09c35ae82dec2
y0329 <kida_hirokazu@tuta.io>
2023-06-20 15:18:29 +0900

    練習第5問　修正

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,15 +11,15 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bonb = pg.Surface((20,20))# 練習1
     pg.draw.circle(bonb,(255,0,0),(10,10),10)# 練習1
-    bonb.set_colorkey((0,0,0))
+    bonb.set_colorkey((0,0,0))# 黒い部分を透明にする
     x = random.randint(0,WIDTH)
     y = random.randint(0,HEIGHT)
-    bonb_rect = bonb.get_rect()
-    bonb_rect.center = x , y
+    bonb_rect = bonb.get_rect()# 爆弾Surface（bonb_img）から爆弾Rect（bonb_rect）を抽出する
+    bonb_rect.center = x , y#中心座標を生成しておいた乱数にする
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rect = kk_img.get_rect()
+    kk_rect = kk_img.get_rect()# こうかとんSurface（kk_img）からこうかとんRect（kk_rect）を抽出する
     kk_rect.center = 900,400
     clock = pg.time.Clock()
     tmr = 0
@@ -27,6 +27,11 @@ def main():
     vy = +5# 練習2
     while True:
         def out_window(rect):
+            """
+            • 引数：こうかとんRect or 爆弾Rect
+            • 戻り値：横方向・縦方向の真理値タプル（True：画面内／False：画面外）
+            • Rectオブジェクトのleft, right, top, bottomの値から画面内・外を判断する
+            """
             vertical,side = True,True
             if rect.left < 0 or WIDTH <rect.right:
                 vertical = False
@@ -37,23 +42,23 @@ def main():
             if event.type == pg.QUIT: 
                 return
         key_lst = pg.key.get_pressed()
-        total_move = [0,0]# 練習3
+        total_move = [0,0]# 合計移動量
         for k,mv in delta.items():
-            if key_lst[k]: 
+            if key_lst[k]:#辞書によって移動量を変更する 
                 total_move[0] += mv[0]
                 total_move[1] += mv[1]
         kk_rect.move_ip(total_move)
         if not out_window(kk_rect)[0] or not out_window(kk_rect)[1]:# 練習4
-            kk_rect.move_ip(-total_move[0],-total_move[1])
+            kk_rect.move_ip(-total_move[0],-total_move[1])#マイナスした座標に移動することでプラスマイナス0にする
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rect)
         bonb_rect.move_ip(vx,vy)# 練習2
         if not out_window(bonb_rect)[0]:# 練習4
-            vx *= -1
+            vx *= -1# 移動方向を反転させる
         elif not out_window(bonb_rect)[1]:#練習4
             vy *= -1
         screen.blit(bonb,bonb_rect)# 練習1
-        if kk_rect.colliderect(bonb_rect):
+        if kk_rect.colliderect(bonb_rect):#こうかとんと爆弾が衝突したら
             return
         pg.display.update()
         tmr += 1
