487929b46d9a439666cd084d76d889e50a8f449b
y0329 <kida_hirokazu@tuta.io>
2023-06-20 14:36:48 +0900

    練習第3問

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {pg.K_UP:(0,-5),pg.K_DOWN:(0,+5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(+5,0)}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -19,24 +19,35 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 900,400
     clock = pg.time.Clock()
     tmr = 0
     vx = +5# 練習2
     vy = +5# 練習2
+    total_move = [0,0]# 練習3
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
         key_lst = pg.key.get_pressed()
-        print(key_lst)
-        bonb_rect.move_ip(vx,vy)
+        for k,mv in delta.items():
+            if key_lst[pg.K_UP]:
+                total_move[1] -=5
+            if key_lst[pg.K_DOWN]:
+                total_move[1] += 5
+            if key_lst[pg.K_LEFT]:
+                total_move[0] -= 5
+            if key_lst[pg.K_RIGHT]:
+                total_move[0] += 5
+            kk_rect.move_ip(total_move[0],total_move[1])
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, kk_rect)
+        bonb_rect.move_ip(vx,vy)# 練習2
         screen.blit(bonb,bonb_rect)# 練習1
         pg.display.update()
         tmr += 1
-        clock.tick(50)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
